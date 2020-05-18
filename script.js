7d173ae6999d1a54ebc2bf8a2ef297d2827ed04b
Devon Navon <devon.navon@gmail.com>
2020-05-18 15:56:12 -0700

    dunn

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const grid = document.querySelector('#grid');
 const display = grid.querySelector('#display');
-const numbers = grid.querySelectorAll('.num')
+const numbers = grid.querySelectorAll('.num');
+const opps = grid.querySelectorAll('.opp');
+const equals = grid.querySelector('#equals');
+const clear = grid.querySelector('#clear');
 
-numbers.forEach((element) => {
-    element.addEventListener('click', updateDisplay);
-});
+let currentValue;
+let currentOpp;
+let newEntryFlag=false;
+
+numbers.forEach((e) => {e.addEventListener('click', updateDisplay);});
+opps.forEach((e) => {e.addEventListener('click', operateDisplay);});
+equals.addEventListener('click',calculate);
+clear.addEventListener('click',clearDisplay);
 
 function updateDisplay (e){
-    if(display.innerHTML==="0") display.innerHTML=e.target.innerHTML;
-    else display.innerHTML+=e.target.innerHTML;
+    if (newEntryFlag){
+        display.innerHTML=e.target.innerHTML
+        newEntryFlag=false;
+    }
+    else{
+        if(display.innerHTML==="0") display.innerHTML=e.target.innerHTML;
+        else display.innerHTML+=e.target.innerHTML;
+    }
+}
+
+function operateDisplay (e){
+    if(currentValue) display.innerHTML = operate(currentOpp, Number(currentValue), Number(display.innerHTML));
+    currentValue = display.innerHTML;
+    currentOpp = e.target.innerHTML;
+    newEntryFlag=true;
+    console.log({currentValue})
+}
+
+function calculate (e){
+    display.innerHTML = operate(currentOpp, Number(currentValue), Number(display.innerHTML))
+    currentValue = undefined;
+    currentOpp = undefined;
+}
+
+function clearDisplay (e){
+    display.innerHTML = 0;
+    currentValue = undefined;
+    currentOpp = undefined;
 }
 
 const oppDict = {
